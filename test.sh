2b7586b03da9f45d7c06383d4949618153a9e015
tubbadu <tubbadu@gmail.com>
2025-02-14 16:34:10 +0100

    rollback

--- test.sh
+++ test.sh
@@ -1,20 +1,15 @@
 echo '
 
+values="0x1A2F3C4B5E6D7A8E 0x9DAB0CDE1F23456A 0x7B8C9D0E1F2A3B4C 0x5E6F7A8B9C0D1E2F 0x4A5B6C7D8E9F0A1B 0x2C3D4E5F6A7B8C9D 0x8E9F0A1B2C3D4E5F 0x1F2A3B4C5D6E7F8A 0x3C4D5E6F7A8B9C0D 0x9E0F1A2B3C4D5E6F 0x7A8B9C0D1E2F3A4B 0x5C6D7E8F9A0B1C2D 0x4E5F6A7B8C9D0E1F 0x2A3B4C5D6E7F8A9B 0x1D2E3F4A5B6C7D8E 0x9B0C1D2E3F4A5B6C 0x8A9B0C1D2E3F4A5B 0x6C7D8E9F0A1B2C3D 0x5E6F7A8B9C0D1E2F 0x3A4B5C6D7E8F9A0B"
 
-
-
-
-#!/bin/sh
-
-values="0x1A 0x2F 0x3C 0x4B 0x5E 0x6D 0x7A 0x8E 0x9D 0xAB"
-
-# Define the variables
-N=10          # Number of iterations
 STARTADDR=0x4000000  # Starting address
 
 for value in $values; do
+    
+    echo "number:  $value"
+    echo "address: $STARTADDR"
     busybox devmem $STARTADDR 64 $value
-
+    
     # Increment the address
     STARTADDR=$((STARTADDR + 8))
     
@@ -27,8 +22,10 @@ busybox devmem 0x4000640 w 0x1
 
 
 for value in $values; do
-    busybox devmem $STARTADDR 64
 
+    echo "address: $STARTADDR"
+    busybox devmem $STARTADDR 64
+    
     # Increment the address
     STARTADDR=$((STARTADDR + 8))
     
