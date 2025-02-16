3e09e3acedc2284c78997117a4950c9d8f62280d
tubbadu <tubbadu@gmail.com>
2025-02-16 21:26:07 +0100

    files

---
+++ src/qemu/include/hw/misc/fft_core.h
@@ -0,0 +1,14 @@
+#ifndef HW_FFT_CORE_H
+#define HW_FFT_CORE_H
+
+	#include "qom/object.h"
+
+	// #if defined(TARGET_AARCH64)
+		#include "hw/arm/virt.h"
+		DeviceState *fft_core_create_arm(const VirtMachineState *vms, int);
+	// #else
+		// DeviceState *fft_core_create_riscv(hwaddr);
+	// #endif
+
+
+#endif
