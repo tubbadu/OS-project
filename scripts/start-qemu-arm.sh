15ba81264f3b8b7c064577661249b77048a8463f
tubbadu <tubbadu@gmail.com>
2025-02-16 21:22:18 +0100

    fix

--- scripts/start-qemu-arm.sh
+++ scripts/start-qemu-arm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd buildroot-arm/output/images/
+QEMU_ARM="../../../qemu/build-arm/aarch64-softmmu/qemu-system-aarch64"
 
 BINARIES_DIR="${0%/*}/"
 # shellcheck disable=SC2164
@@ -27,6 +28,7 @@ if ! ${mode_sys_qemu}; then
     export PATH="/mnt/buildroot-arm/output/host/bin:${PATH}"
 fi
 
+echo "QEMU_ARM = $QEMU_ARM"
 # exec qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -kernel Image -append "rootwait root=/dev/vda console=ttyAMA0" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0  ${EXTRA_ARGS} "$@"
 # exec $QEMU_ARM -M virt -bios fw_jump.bin -kernel Image -append "rootwait root=/dev/vda ro" -drive file=rootfs.ext2,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -netdev user,id=net0 -device virtio-net-device,netdev=net0 -nographic
 # exec $QEMU_ARM -M virt -kernel Image -append "rootwait root=/dev/vda ro" -drive file=rootfs.ext2,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -netdev user,id=net0 -device virtio-net-device,netdev=net0 -nographic
