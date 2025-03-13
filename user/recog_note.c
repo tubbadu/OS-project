c050ab66312a4521c1818d61ea5a1eac0b6cc494
Cesacesa <jajoce01@gmail.com>
2025-03-13 15:06:29 +0100

    function to recognize notes

---
+++ user/recog_note.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+
+const char* recog_note(double frequency){
+    int semitone;
+    const char *noteNames[] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+    semitone=round(12*log2(frequency/440));
+    if(abs(semitone)>11){
+        semitone=semitone%12;
+    }
+    printf("The note is:\n");
+    if(semitone>=0){
+        return noteNames[semitone];
+    }else{
+        return noteNames[12+semitone];
+    }
+}
+
+
+int main(){
+    const char*note;
+    double frequency;
+    printf("Insert a frequency (Hz): \n");
+
+    scanf("%lf", &frequency);
+    
+    note=recog_note(frequency);
+    
+    printf("%s", note);
+}
