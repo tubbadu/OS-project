85efbfa2a1ef9800da84373d6be252bfc5cd798d
Cesacesa <jajoce01@gmail.com>
2025-03-13 15:42:21 +0100

    header for recog_note

--- test/recog_note.h
+++ test/recog_note.h
@@ -1,10 +1,11 @@
 #ifndef RECOG_NOTE_H_  
+#define RECOG_NOTE_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-#define RECOG_NOTE_H_
+
 
 const char* recog_note(int frequency);  
 #endif //RECOG_NOTE_H_
