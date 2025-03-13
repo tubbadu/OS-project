a2e726a6c3cbc3940ea1da3bdd671418b45fc405
Cesacesa <jajoce01@gmail.com>
2025-03-13 15:30:44 +0100

    created header

--- test/recog_note.c
+++ test/recog_note.c
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-
-
-
 const char* recog_note(int frequency){
     int semitone;
     const char *noteNames[] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
@@ -13,24 +5,9 @@ const char* recog_note(int frequency){
     if(abs(semitone)>11){
         semitone=semitone%12;
     }
-    printf("The note is:\n");
     if(semitone>=0){
         return noteNames[semitone];
     }else{
         return noteNames[12+semitone];
     }
 }
-
-/*
-int main(){
-    const char*note;
-    double frequency;
-    printf("Insert a frequency (Hz): \n");
-
-    scanf("%lf", &frequency);
-    
-    note=recog_note(frequency);
-    
-    printf("%s", note);
-}   
-*/
---
+++ test/recog_note.h
@@ -0,0 +1,10 @@
+#ifndef RECOG_NOTE_H_  
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define RECOG_NOTE_H_
+
+const char* recog_note(int frequency);  
+#endif //RECOG_NOTE_H_
