3fe976390ff24f79af99b322aad688fc10829813
tubbadu <tubbadu@gmail.com>
2025-03-14 11:38:17 +0100

    fixed path export in mkenv

--- scripts/mkenv.sh
+++ scripts/mkenv.sh
@@ -1,7 +1,7 @@
 
 
 cat <<EOF > .env
-export PATH=$(echo "$PATH" | tr -d ' \t\n')
+export PATH=\$(echo "\$PATH" | tr -d ' \t\n')
 BASEDIR="$(pwd)"
 QEMU_ARM="\$BASEDIR/qemu/build-arm/aarch64-softmmu/qemu-system-aarch64"
 QEMU_RISCV="\$BASEDIR/qemu/build-riscv/riscv64-softmmu/qemu-system-riscv64"
